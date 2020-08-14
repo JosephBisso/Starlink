771a5d1fbb9e5e81d56ea303082375b3e424374a
Klemens Köring <k.koering@tu-bs.de>
2020-08-14 13:54:45 +0200

    Graphen angepasst
    Der Quellcode vom Graphen wurde bei jedem Land aktualisiert

--- src/ungarn.cpp
+++ src/ungarn.cpp
@@ -92,34 +92,40 @@ Ungarn::~Ungarn()
 
 void Ungarn::on_buttonBox_clicked(QAbstractButton *button)
 {
-    button->isChecked();
-    ui->progressBar->setValue(15);
+    button->isChecked(); //Dies erfüllt keinen Zweck und dient nur dazu die Fehlermeldung wegen des
+                         //unbenutzten  button zu schweigen.
 
-    qDebug ("ApplyChange Starts...");
+    ui->progressBar->setValue(15); //Schaut dem Nutzer den Stand der Operation
+
+    qDebug ("ApplyChange Starts..."); // qDebug dient mir nur dazu mich über den Verlauf zu informieren
 
 
-    QDate uiDatum = ui->dateEdit->date();
+    QDate uiDatum = ui->dateEdit->date(); //Das aktuelles Datum auf dem UI wird gelesen und in einen Variable gespeichert
 
-    if (uiDatum>QDate::currentDate())
+    if (uiDatum>QDate::currentDate()) //Prüft ob das Datum sich nich in der Zukunft befindet
     {
         ui->progressBar->setValue(100);
         QMessageBox::information(this, "ungültiges Datum", "Daten werden nur darstellt und nicht vorausgesagt.");
 
-        ui->lineEdit_4->setText("");
+        ui->lineEdit_4->setText(""); //Zeilen werden leer gemacht
         ui->lineEdit_6->setText("");
 
         ui->progressBar->setValue(0);
     }
 
-    else
+    else //Wenn das Datum das aktuelles oder en vergangenes ist:
     {
         ui->progressBar->setValue(45);
 
+        //Land und geoID sind Attribute dieser Klasse.
 
+        Land.FillLines(uiDatum, geoID); //Die Methode aus der Klasser Laender FillLines wird gerufen
+                                       // es werden die Anzahl von Infiziierten und Toden an dem
+                                      //vom User ausgewählten Datum dargestellt. Die Daten werden anschließend
+                                     //in dem Attribut Infi und Tode vom Objekt Land
 
-        Land.FillLines(uiDatum, geoID);
-
-        if (Land.Infi == "-999" || Land.Tode == "-999")
+        if (Land.Infi == "-999" || Land.Tode == "-999") //(-999 wird von der Methode FillLines zurückgegeben,
+                                              //wenn keine Daten zu dem Datum gefunden wurden)
         {
             ui->progressBar->setValue(100);
 
@@ -127,75 +133,134 @@ void Ungarn::on_buttonBox_clicked(QAbstractButton *button)
                                                                " nicht gefunden. Versuchen "
                                                                "Sie die Datenbank zu aktualisieren");
 
-            ui->lineEdit_4->setText("");
+            ui->lineEdit_4->setText(""); //Zeilen leer gemacht
             ui->lineEdit_6->setText("");
 
             ui->progressBar->setValue(0);
         }
 
-        else
+        else //Wenn alle vorherigen Bedingungen gut klappen, dann werden die Daten dargestellt.
         {
-            ui->tab->layout()->~QLayout();
+            ui->tab->layout()->~QLayout(); //das aktuelles Layout im Tab wird gelöscht
+            ui->tab_5->layout()->~QLayout();
 
             ui->progressBar->setValue(75);
 
 
-        ui->lineEdit_4->setText(Land.Infi);
+        ui->lineEdit_4->setText(Land.Infi); //Das Ergebnis die durch der Methode im Attribut Infi vom
+                                           // Objekt Land gespeichert wurde wird gerufen
         ui->lineEdit_6->setText(Land.Tode);
 
 
 
-        QLineSeries *seriesInf = new QLineSeries(),
+        QLineSeries *seriesInf = new QLineSeries(), //Es werden neue Serien geschaffen, dank dessen eine Graph
+                                                   //die Serie bekommen nachher Koordinate hinzugefügt
                    *seriesTode = new QLineSeries();
-        seriesInf->setName("Infiziierte");
+        seriesInf->setName("Infiziierte");//Die Serien bekommen einen Namen. Ist für die Legende wichtig
         seriesTode->setName("Tode");
 
-        QString Titel = "Übersicht für xxx von 01 bis yyy"; //Der Titel der Graph
-        QString tblMonat = Land.DbLandDaten.gibMonat(Land.DbLandDaten.Monat);
+        QString Titel = "Übersicht Infiziierte für xxx"; //Der Titel der Graph
+        QString tblMonat = Land.DbLandDaten.gibMonat(Land.DbLandDaten.Monat); //Die Mehtode gib aus den Monat im Format
+                                                                             //"mm" der im Hintergrund durch die
+                                                                            //Methode FillLines im Attribut Monat vom
+                                                                           //Attribut DbLandDaten vom Objet Land
+                                                                          //gespeichert. Lange Kette zwar, aber
+                                                                         //effektiv.
         Titel.replace("xxx", tblMonat);
-        Titel.replace("yyy", Land.DbLandDaten.Datum);
+        QString TitelTode = Titel;
+        TitelTode.replace("Infiziierte", "Tode");
 
 
 
-        int n = Land.FillTab(uiDatum, Land.skalaLinear, geoID);
+        int n = Land.FillTab(uiDatum, Land.skalaLinear, geoID); //Die Methode FillTab wird gerufen. Es werden Daten
+                                                               // von allen Tagen im Monat vom ersten Tag bis zum
+                                                              //vom User ausgewählten Tag gerufen und in 2 Matrixen,
+                                                             //die Attribute der Klasse Laender sind, gespeichert.
+                                                            //SkalaLinear bestimmt, ob die Daten liniar oder logarith-
+                                                           //misch gespeichert werden soll.
+                                                          //Die Methode gib die Anzahl von Tagen zurück, für die
+                                                         //Daten gerufen wurden
+
+        if (n<uiDatum.daysInMonth()) //Prüft, ob die Anzahl von Einträge (n) kleiner ist als die Anzahl an Tagen in des ausgewählten Monats
+        {
+            QMessageBox::information(this, "Datenbank", "Es fehlen anscheinend Daten für einen Teil dises Monats");
+
+            QString TagEnde = QString::number(n);
+            if (TagEnde.size()==1){TagEnde.insert(0, "0");}
+
+            QString DatumEnde = Land.DbLandDaten.gibDatum(TagEnde, Land.DbLandDaten.Monat); //Das letzte in der Datenbank existierende Datum für
+                                                                                           // Monat wir gerufen und gespeichert...
+
+            Titel.append(" bis "); Titel.append(DatumEnde); //... Und wird dem Titel hinzugefügt
+            TitelTode.append(" bis "); TitelTode.append(DatumEnde);
+        }
 
         for (int i=0; i<n;i++)
         {
-            seriesInf->append( Land.tblInfi[0][n-1-i], Land.tblInfi[1][n-1-i]);
+            seriesInf->append( Land.tblInfi[0][i], Land.tblInfi[1][i]); //In der erste Spalte der Matrix tblInfi
+                                                                               //werden die Nummer von den Tagen und
+                                                                              //in der 2. Spalte die Daten gespeichert.
+                                                                             //Sie werden der Serie als Koordinate
+                                                                            //hinzugefügt. Die Schleife geht durch die
+                                                                           //Matrixen rückwärts von n-1 bis 1.
 
             qDebug()<< "Punkt Nummer i = "<<i<<" = "
-                    << "("<<Land.tblInfi[0][n-1-i]<<","<<Land.tblInfi[1][n-1-i]<<")" ;
+                    << "("<<Land.tblInfi[0][i]<<","<<Land.tblInfi[1][i]<<")" ;
 
-            seriesTode->append(Land.tblTode[0][n-1-i], Land.tblTode[1][n-1-i]);
+            seriesTode->append(Land.tblTode[0][i], Land.tblTode[1][i]);
         }
 
         ui->progressBar->setValue(87);
 
-        QChart *chart = new QChart();
+        QChart *chart = new QChart(), //Eine neue Instanz vom Typ QChart wird gerufen. Sie bekommen eine Legende, einen
+                                     //Titel und den Series hinzugefügt
+               *chartTode = new QChart();
         chart->legend()->setVisible(true);
         chart->addSeries(seriesInf);
-        chart->addSeries(seriesTode);
         chart->setTitle(Titel);
+        chartTode->legend()->setVisible(true);
+        chartTode->addSeries(seriesTode);
+        chartTode->setTitle(TitelTode);
+        chartTode->setTheme(QChart::ChartThemeDark);
 
-        QValueAxis *axisX = new QValueAxis();
+        QValueAxis *axisX = new QValueAxis(), //Axe x
+                *axisXTode = new QValueAxis();
         axisX->setTickCount(n);
+        axisX->setLabelFormat("%.0f");
         chart->addAxis(axisX, Qt::AlignBottom);
-        seriesInf->attachAxis(axisX);
+        seriesInf->attachAxis(axisX); // Die Serie wird mit der Achse verbunden
+        axisXTode->setTickCount(n);
+        axisXTode->setLabelFormat("%.0f"); //Damit es keine Gleitkommazahlen gibt
+        chartTode->addAxis(axisXTode, Qt::AlignBottom);
+        seriesTode->attachAxis(axisXTode);
 
-        QValueAxis *axisYInf = new QValueAxis();
+        qDebug() << "Label Format für Axis X "<<axisXTode->labelFormat();
+
+        QValueAxis *axisYInf = new QValueAxis(),
+                *axisYTode = new QValueAxis();
         chart->addAxis(axisYInf, Qt::AlignLeft);
         seriesInf->attachAxis(axisYInf);
-        seriesTode->attachAxis(axisYInf);
-        axisYInf->setMin(0);
+        axisYInf->setMin(0); //Es wird ein Minimun gesetzt.
+        chartTode->addAxis(axisYTode, Qt::AlignLeft);
+        seriesTode->attachAxis(axisYTode);
+        axisYTode->setMin(0);
 
-        QChartView *chartView = new QChartView(chart);
+        QChartView *chartView = new QChartView(chart),// Zur Darstellung der Graphen
+                *chartViewTode = new QChartView(chartTode);
         chartView->setRenderHint(QPainter::Antialiasing);
+        chartViewTode->setRenderHint(QPainter::Antialiasing);
 
         ui->progressBar->setValue(92);
 
-        QVBoxLayout* verticalLayout_3 = new QVBoxLayout(ui->tab);
+        QVBoxLayout* verticalLayout_3 = new QVBoxLayout(ui->tab), //Ein neues Layout wird erstellt und an ui->Tab
+                                                                 //gegeben. Das Layout bekommt dann den Chart
+                                                                //hinzugefügt
+                *verticalLayout_3Tode = new QVBoxLayout(ui->tab_5);
+
         verticalLayout_3->addWidget(chartView);
+        verticalLayout_3Tode->addWidget(chartViewTode);
         ui->tab->setLayout(verticalLayout_3);
+        ui->tab_5->setLayout(verticalLayout_3Tode);
 
         ui->progressBar->setValue(100);
 
