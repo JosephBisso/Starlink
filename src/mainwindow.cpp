f85bc71476eb82dfa8b6fdf7009a4fbe086a3c5e
Joseph Loic Bisso-Bi-Ela <j.bisso-bi-ela@tu-braunschweig.de>
2021-04-03 20:08:41 +0200

    Icon beim Öffnen initialisieren

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -83,6 +83,10 @@ void MainWindow::on_Frankreich_clicked()
     Land* land = new Land(this);
     land->setGeoID("FR");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
+
     land->exec();
 }
 
@@ -91,6 +95,10 @@ void MainWindow::on_Deutschland_clicked()
     Land* land = new Land(this);
     land->setGeoID("DE");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
+
     land->exec();
 }
 
@@ -99,6 +107,9 @@ void MainWindow::on_Spanien_clicked()
     Land* land = new Land(this);
     land->setGeoID("ES");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -107,6 +118,9 @@ void MainWindow::on_Portugal_clicked()
     Land* land = new Land(this);
     land->setGeoID("PT");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -115,6 +129,9 @@ void MainWindow::on_Belgien_clicked()
     Land* land = new Land(this);
     land->setGeoID("BE");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -123,6 +140,9 @@ void MainWindow::on_Niederlande_clicked()
     Land* land = new Land(this);
     land->setGeoID("NL");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -131,6 +151,9 @@ void MainWindow::on_Schweiz_clicked()
     Land* land = new Land(this);
     land->setGeoID("CH");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -139,6 +162,9 @@ void MainWindow::on_Oesterreich_clicked()
     Land* land = new Land(this);
     land->setGeoID("AT");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -147,6 +173,9 @@ void MainWindow::on_Grossbritannien_clicked()
     Land* land = new Land(this);
     land->setGeoID("UK");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -155,6 +184,9 @@ void MainWindow::on_Irland_clicked()
     Land* land = new Land(this);
     land->setGeoID("IE");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -163,6 +195,9 @@ void MainWindow::on_Island_clicked()
     Land* land = new Land(this);
     land->setGeoID("IS");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -171,6 +206,9 @@ void MainWindow::on_Polen_clicked()
     Land* land = new Land(this);
     land->setGeoID("PL");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -179,6 +217,9 @@ void MainWindow::on_Tschechien_clicked()
     Land* land = new Land(this);
     land->setGeoID("CZ");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -187,6 +228,9 @@ void MainWindow::on_Slowakei_clicked()
     Land* land = new Land(this);
     land->setGeoID("SK");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -195,6 +239,9 @@ void MainWindow::on_Ungarn_clicked()
     Land* land = new Land(this);
     land->setGeoID("HU");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -203,6 +250,9 @@ void MainWindow::on_Italien_clicked()
     Land* land = new Land(this);
     land->setGeoID("IT");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -211,6 +261,9 @@ void MainWindow::on_Slowenien_clicked()
     Land* land = new Land(this);
     land->setGeoID("SI");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -219,6 +272,9 @@ void MainWindow::on_Kroatien_clicked()
     Land* land = new Land(this);
     land->setGeoID("HR");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -227,6 +283,9 @@ void MainWindow::on_Montenegro_clicked()
     Land* land = new Land(this);
     land->setGeoID("ME");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -235,6 +294,9 @@ void MainWindow::on_Kosovo_clicked()
     Land* land = new Land(this);
     land->setGeoID("XK");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -243,6 +305,9 @@ void MainWindow::on_Serbien_clicked()
     Land* land = new Land(this);
     land->setGeoID("RS");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -251,6 +316,9 @@ void MainWindow::on_Rumanien_clicked()
     Land* land = new Land(this);
     land->setGeoID("RO");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -259,6 +327,9 @@ void MainWindow::on_Albanien_clicked()
     Land* land = new Land(this);
     land->setGeoID("AL");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -267,6 +338,9 @@ void MainWindow::on_Mazedonien_clicked()
     Land* land = new Land(this);
     land->setGeoID("MK");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -275,6 +349,9 @@ void MainWindow::on_Bulgarien_clicked()
     Land* land = new Land(this);
     land->setGeoID("BG");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -283,6 +360,9 @@ void MainWindow::on_Griechenland_clicked()
     Land* land = new Land(this);
     land->setGeoID("EL");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -291,6 +371,9 @@ void MainWindow::on_Moldawien_clicked()
     Land* land = new Land(this);
     land->setGeoID("MD");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -299,6 +382,9 @@ void MainWindow::on_Ukraine_clicked()
     Land* land = new Land(this);
     land->setGeoID("UA");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -307,6 +393,9 @@ void MainWindow::on_Weissrussland_clicked()
     Land* land = new Land(this);
     land->setGeoID("BY");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -315,6 +404,9 @@ void MainWindow::on_Daenemark_clicked()
     Land* land = new Land(this);
     land->setGeoID("DK");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -323,6 +415,9 @@ void MainWindow::on_Norwegen_clicked()
     Land* land = new Land(this);
     land->setGeoID("NO");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -331,6 +426,9 @@ void MainWindow::on_Schweden_clicked()
     Land* land = new Land(this);
     land->setGeoID("SE");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -339,6 +437,9 @@ void MainWindow::on_Finnland_clicked()
     Land* land = new Land(this);
     land->setGeoID("FI");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -347,6 +448,9 @@ void MainWindow::on_Estland_clicked()
     Land* land = new Land(this);
     land->setGeoID("EE");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -355,6 +459,9 @@ void MainWindow::on_Lettland_clicked()
     Land* land = new Land(this);
     land->setGeoID("LV");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -363,6 +470,9 @@ void MainWindow::on_Litauen_clicked()
     Land* land = new Land(this);
     land->setGeoID("LT");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
 
@@ -371,5 +481,8 @@ void MainWindow::on_Bosnien_clicked()
     Land* land = new Land(this);
     land->setGeoID("BA");
     land->setWindowTitle(land->setTitle());
+
+    QIcon* icon = new QIcon (*land->setIcon());
+    land->setWindowIcon(*icon);
     land->exec();
 }
